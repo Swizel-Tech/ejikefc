5606aaed19eca1883cf5fa5110371c8ff2560deb
RehkMansa <godspowernathaniel25@gmail.com>
2022-10-01 22:52:56 +0100

    modified staff team

---
+++ staffs.php
@@ -0,0 +1,196 @@
+<?php include "base/header.php" ?>
+<style>
+  .card{
+    background-color: var(--bgDark)
+  }
+</style>
+<section class="breadcrumb breadcrumb_bg">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="breadcrumb_iner">
+          <div class="breadcrumb_iner_item">
+            <h1>Contact Us</h1>
+            <p>Home<span>/</span>Contact Us</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="team_member section_padding padding_less_40">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 col-lg-3">
+        <div class="single_team_member single-home-blog">
+          <div class="card">
+            <img
+              src="img/team/team_1.png"
+              class="card-img-top"
+             alt="staff team"
+              data-pagespeed-url-hash="4080457325"
+              onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+            />
+            <div class="card-body">
+              <div class="tean_content">
+                <a href="blog.html">
+                  <h5 class="card-title">Rocky Benard</h5>
+                </a>
+                <p>Staff</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3">
+        <div class="single_team_member single-home-blog">
+          <div class="card">
+            <img
+              src="img/team/team_2.png"
+              class="card-img-top"
+             alt="staff team"
+              data-pagespeed-url-hash="79989950"
+              onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+            />
+            <div class="card-body">
+              <div class="tean_content">
+                <a href="blog.html">
+                  <h5 class="card-title">Adult Malerd</h5>
+                </a>
+                <p>Staff</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3">
+        <div class="single_team_member single-home-blog">
+          <div class="card">
+            <img
+              src="img/team/team_3.png"
+              class="card-img-top"
+             alt="staff team"
+              data-pagespeed-url-hash="374489871"
+              onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+            />
+            <div class="card-body">
+              <div class="tean_content">
+                <a href="blog.html">
+                  <h5 class="card-title">Rocky Benard</h5>
+                </a>
+                <p>Staff</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3">
+        <div class="single_team_member single-home-blog">
+          <div class="card">
+            <img
+              src="img/team/team_4.png"
+              class="card-img-top"
+             alt="staff team"
+              data-pagespeed-url-hash="668989792"
+              onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+            />
+            <div class="card-body">
+              <div class="tean_content">
+                <a href="blog.html">
+                  <h5 class="card-title">Jimmy Cartoon</h5>
+                </a>
+                <p>Staff</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3">
+        <div class="single_team_member single-home-blog">
+          <div class="card">
+            <img
+              src="img/team/team_1.png"
+              class="card-img-top"
+             alt="staff team"
+              data-pagespeed-url-hash="4080457325"
+              onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+            />
+            <div class="card-body">
+              <div class="tean_content">
+                <a href="blog.html">
+                  <h5 class="card-title">Jimmy Cartoon</h5>
+                </a>
+                <p>Staff</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3">
+        <div class="single_team_member single-home-blog">
+          <div class="card">
+            <img
+              src="img/team/team_2.png"
+              class="card-img-top"
+             alt="staff team"
+              data-pagespeed-url-hash="79989950"
+              onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+            />
+            <div class="card-body">
+              <div class="tean_content">
+                <a href="blog.html">
+                  <h5 class="card-title">Adult Malerd</h5>
+                </a>
+                <p>Staff</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3">
+        <div class="single_team_member single-home-blog">
+          <div class="card">
+            <img
+              src="img/team/team_3.png"
+              class="card-img-top"
+             alt="staff team"
+              data-pagespeed-url-hash="374489871"
+              onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+            />
+            <div class="card-body">
+              <div class="tean_content">
+                <a href="blog.html">
+                  <h5 class="card-title">Rocky Benard</h5>
+                </a>
+                <p>Staff</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3">
+        <div class="single_team_member single-home-blog">
+          <div class="card">
+            <img
+              src="img/team/team_4.png"
+              class="card-img-top"
+             alt="staff team"
+              data-pagespeed-url-hash="668989792"
+              onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+            />
+            <div class="card-body">
+              <div class="tean_content">
+                <a href="blog.html">
+                  <h5 class="card-title">Rocky Benard</h5>
+                </a>
+                <p>Staff</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include "base/footer.php" ?>
